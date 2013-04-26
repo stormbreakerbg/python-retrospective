625075564fb585fdcdc0dbfb81e6fc7ca72bd57a
Georgy Angelov <georgyangelov@gmail.com>
2013-04-26 09:45:01 +0300

    Shorten TicTacToeBoard's code using string keys

--- task4/solution.py
+++ task4/solution.py
@@ -8,53 +8,40 @@ class TicTacToeBoard:
                    '  -------------\n' +\
                    '    A   B   C  \n'
 
-    __WIN_LINES = (
-        ((1, 1), (1, 0)),
-        ((1, 1), (0, 1)),
-        ((1, 1), (1, 1)),
-        ((3, 1), (-1, 1)),
-        ((3, 1), (0, 1)),
-        ((3, 3), (-1, 0)),
-        ((2, 1), (0, 1)),
-        ((3, 2), (-1, 0)),
+    _WIN_LINES = (
+        ('A1', 'B2', 'C3'),
+        ('A1', 'B1', 'C1'),
+        ('A1', 'A2', 'A3'),
+        ('B1', 'B2', 'B3'),
+        ('C1', 'C2', 'C3'),
+        ('A2', 'B2', 'C2'),
+        ('A3', 'B3', 'C3'),
+        ('C1', 'B2', 'A3')
     )
 
     def __init__(self):
-        self.__game_board = {(row, column): ' '
-                             for row in [1, 2, 3]
-                             for column in [1, 2, 3]}
+        self._game_board = {column + row: ' '
+                            for row in ['1', '2', '3']
+                            for column in ['A', 'B', 'C']}
         self._next_turn = None
         self._winner = None
 
-    def __check_line(self, start_index, delta):
-        """Check if the line is complete
-        and return the player to whom it belongs, else return None.
-        """
-
-        position = start_index
-        player = None
-
-        while 1 <= position[0] <= 3 and 1 <= position[1] <= 3:
-            if player is not None and self.__game_board[position] != player:
-                return None
-
-            player = self.__game_board[position]
-            position = position[0] + delta[0], position[1] + delta[1]
-
-        return player if player is not ' ' else None
-
     def __check_win(self):
         """Check if any player won the game and return X, O or None."""
-        for win_line in self.__WIN_LINES:
-            check_result = self.__check_line(*win_line)
-            if check_result is not None:
-                return check_result
+        for win_line in self._WIN_LINES:
+            line_values = set(self._game_board[key] for key in win_line)
+
+            if len(line_values) == 1:
+                value = line_values.pop()
+
+                if value != ' ':
+                    return value
 
         return None
 
     def __all_filled(self):
         """Check if all the fields have been filled."""
-        return all(field != ' ' for field in self.__game_board.values())
+        return all(field != ' ' for field in self._game_board.values())
 
     def game_status(self):
         if self._winner is not None:
@@ -66,20 +53,15 @@ class TicTacToeBoard:
         return "Game in progress."
 
     @staticmethod
-    def _parse_index(index):
+    def _check_index(index):
         """Return tuple of positions (row, column) in numbers from 1 to 3."""
 
         if len(index) != 2:
             raise InvalidKey("The length of the key must be 2 symbols")
 
-        column = ord(index[0]) - ord('A') + 1
-        row = int(index[1])
-
-        if not 1 <= column <= 3 or not 1 <= row <= 3:
+        if index[0] not in ['A', 'B', 'C'] or index[1] not in ['1', '2', '3']:
             raise InvalidKey("The first symbol should be A, B or C, and the "
                              "second one should be 1, 2 or 3.")
-
-        return (row, column)
 
     def __getitem__(self, index):
         """Return board data at `index`.
@@ -87,9 +69,9 @@ class TicTacToeBoard:
         `index` should be one letter A, B or C, followed by 1, 2 or 3.
         Raises InvalidKey if the format isn't valid.
         """
-        index_tuple = TicTacToeBoard._parse_index(index)
+        TicTacToeBoard._check_index(index)
 
-        return self.__game_board[index_tuple]
+        return self._game_board[index]
 
     def __setitem__(self, index, value):
         if value not in ('X', 'O'):
@@ -99,19 +81,17 @@ class TicTacToeBoard:
             raise NotYourTurn("The {} player should play this turn"
                               .format(self._next_turn))
 
-        index_tuple = TicTacToeBoard._parse_index(index)
+        TicTacToeBoard._check_index(index)
 
-        if self.__game_board[index_tuple] != ' ':
+        if self._game_board[index] != ' ':
             raise InvalidMove("The field {} is already set.".format(index))
 
-        self.__game_board[index_tuple] = value
+        self._game_board[index] = value
         self._next_turn = 'X' if value == 'O' else 'O'
         self._winner = self.__check_win()
 
     def __str__(self):
-        board_data = {chr(column + ord('A') - 1) + str(row): value
-                      for (row, column), value in self.__game_board.items()}
-        return self.BOARD_FORMAT.format(**board_data)
+        return self.BOARD_FORMAT.format(**self._game_board)
 
 
 class InvalidKey(Exception):
